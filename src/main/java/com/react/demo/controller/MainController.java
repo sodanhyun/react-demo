0dfc5befd6df5ee52e7a8607e37064b6f81de6cc
danhyeon <166380533+sodanhyun@users.noreply.github.com>
2024-04-17 00:30:32 +0900

    .

--- src/main/java/com/react/demo/controller/MainController.java
+++ src/main/java/com/react/demo/controller/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @CrossOrigin("http://localhost:3000")
     @GetMapping("/api/main")
     public String getMain() {
         return "Hello World";
