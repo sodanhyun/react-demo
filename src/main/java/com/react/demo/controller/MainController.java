4813badf78ab4641dc1e74a014401b048f387f07
danhyeon <166380533+sodanhyun@users.noreply.github.com>
2024-04-16 22:14:19 +0900

    Initial commit

---
+++ src/main/java/com/react/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.react.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @CrossOrigin("http://localhost:3000")
+    @GetMapping("/main")
+    public String getMain() {
+        return "Hello World";
+    }
+
+}
