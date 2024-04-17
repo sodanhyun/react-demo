af3880db5e43793e915785ffb69d7ac97267acf3
danhyeon <sodanhyun@gmail.com>
2024-04-17 15:02:35 +0900

    .

--- src/main/java/com/react/demo/ReactDemoApplication.java
+++ src/main/java/com/react/demo/ReactDemoApplication.java
@@ -13,4 +13,14 @@ public class ReactDemoApplication {
 		SpringApplication.run(ReactDemoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
 }
--- src/main/java/com/react/demo/controller/MainController.java
+++ src/main/java/com/react/demo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.react.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/api/main")
+    @GetMapping("/main")
     public String getMain() {
         return "Hello World";
     }
